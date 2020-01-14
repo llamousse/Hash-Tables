ab89da7cb896539c00a48f38eef961c2c1a0d77e
Vicky <vyue013@gmail.com>
2020-01-14 15:56:49 -0800

    mvp complete

--- src/hashtable.py
+++ src/hashtable.py
@@ -69,7 +69,20 @@ class HashTable:
         Print a warning if the key is not found.
         Fill this in.
         '''
-        pass
+
+        hashed_key = self._hash_mod(key)
+        storage_key = self.storage[hashed_key]
+        
+        while storage_key is not None and storage_key.key != key:
+            storage_key = storage_key.next
+        if storage_key is None:
+            return None
+        else:
+            self.count -= 1
+            res = storage_key.value
+            storage_key.value = None
+        return res
+
 
     def retrieve(self, key):
         '''
@@ -132,47 +145,3 @@ if __name__ == "__main__":
     print(ht.retrieve("line_3"))
 
     print("")
-
-
-# lecture notes
-#
-# class DynamicArray:
-#     def __init__(self, capacity=8):
-#         self.count = 0  # Count is how much is currently used
-#         self.capacity = capacity  # How much is currently allocated
-#         self.storage = [None] * self.capacity
-
-#     def insert(self, index, value):
-#         if self.count == self.capacity:
-#             self.resize()
-#             return
-#         # Shift everything to the right
-#         for i in range(self.count, index, -1):
-#             self.storage[i] = self.storage[i - 1]
-#         # Insert our value
-#         self.storage[index] = value
-#         self.count += 1
-
-#     def append(self, value):
-#         self.insert(self.count, value)
-
-#     def resize(self):
-#         self.capacity *= 2
-#         new_storage = [None] * self.capacity
-#         for i in range(self.count):
-#             new_storage[i] = self.storage[i]
-#         self.storage = new_storage
-
-#     def replace(self, index, value):
-#         self.storage[index] = value
-
-#     def add_to_front(self, value):
-#         self.insert(0, value)
-
-#     def slice(self, beginning_index, end_index):  # default value
-#         # beginning and end
-#         # create subarray to store value
-#         # copy beginning  to end to subarray
-#         # decide how this works.  What happens  to the original array?
-#         # leave it alone?  Or cut out what  we're slicing
-#         # return subarray
